f4e17a3169b5a3dfcbecf8a592e0a67713850961
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-03-05 00:51:39 +0300

    Add files via upload

--- LW1code.sql
+++ LW1code.sql
@@ -5,6 +5,7 @@ set autoprint on
 CREATE PROCEDURE show_info
 IS
     VERS VARCHAR(30) := '';
+    ROWNUM INT := 0;
 
 BEGIN
     SELECT version into VERS from V$INSTANCE;
@@ -19,7 +20,8 @@ BEGIN
         dbms_output.put_line(' ');
 
         FOR vers_row IN (SELECT ID, action, action_time, VERSION FROM SYS.REGISTRY$HISTORY) LOOP
-                dbms_output.put_line(RPAD(vers_row.ID, 4) || ' ' || RPAD(vers_row.ACTION_TIME, 17) || ' ' ||
+                ROWNUM := ROWNUM + 1;
+                dbms_output.put_line(RPAD(ROWNUM, 4) || ' ' || RPAD(vers_row.ACTION_TIME, 17) || ' ' ||
                                      RPAD(' ', 21) || ' ' || RPAD(vers_row.VERSION, 26));
         END LOOP;
 	END IF;
